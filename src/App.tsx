a07d72d84956e8f061a1559e7d29de0636fc57a4
Jarosław K <jarasss1@wp.pl>
2025-04-01 19:33:54 +0200

    feat: initialize project with Vite, add Sass, React Router, Formik, and Vitest

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Tu powstaje:</h2>
+      <h1>Wsparcie Dla Ciebie </h1>
+      <p> Rozmawiaj z chatbotem terapeutycznym</p>
+    </>
+  )
+}
+
+export default App
