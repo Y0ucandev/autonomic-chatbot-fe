0957159d85cc671cc4f709d3092621c390f06daa
Jarosław K <jarasss1@wp.pl>
2025-04-02 21:39:36 +0200

    feat: setup routes, header, and navigation style:  define global styling variables for colors and font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 
+import { Outlet, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Header from './Components/Header/Header'
+import Home from './Components/Home/Home'
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
 
 function App() {
 
   return (
-    <>
-      <h2>Tu powstaje:</h2>
-      <h1>Wsparcie Dla Ciebie </h1>
-      <p> Rozmawiaj z chatbotem terapeutycznym</p>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes >
+
   )
 }
 
