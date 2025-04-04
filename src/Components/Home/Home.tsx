28963b4e212895aa1a931bd35234bbc59f79a99c
Jarosław K <jarasss1@wp.pl>
2025-04-04 15:27:24 +0200

    feat: Home, SupportYou

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,8 +1,26 @@
-
+import SupportYou from '../SupportYou/SupportYou'
+import Style from './Home.module.scss'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <>
+            <div className={Style.home}>
+                <div className={Style.wrapTitle}>
+                    <h1 className={Style.title}>TWÓJ CYFROWY TERAPEUTA</h1>
+                    <h2>WSPARCIE NA WYCIĄGNIĘCIE RĘKI</h2>
+                </div>
+                <div className={Style.btnChat}>
+                    <a href="#">ROZPOCZNIJ ROZMOWĘ</a>
+                </div>
+                <div className={Style.animation}>
+                    {/* <img src="../../../public/animationWomen.gif" alt="" /> */}
+                </div>
+
+            </div>
+            <SupportYou />
+
+        </>
+
     )
 }
 
---
+++ src/Components/SupportYou/SupportYou.tsx
@@ -0,0 +1,14 @@
+
+import Style from './SupportYou.module.scss'
+
+const SupportYou = () => {
+    return (
+        <div className={Style.supportYou}>
+            <div className={Style.title}>  <a href="#" className={Style.imgChat}></a>
+                <h2>JAK MOGĘ CI POMÓC</h2></div>
+
+        </div>
+    )
+}
+
+export default SupportYou
